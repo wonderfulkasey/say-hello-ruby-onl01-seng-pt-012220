c25482f8673d98e7eefd2c1eecac8ee91d71bb85
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-22 02:09:46 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build your say_hello method here
 
 def say_hello(name)
-  puts "Hello #{name}"
+  puts "Hello #{name}!"
 end
 
 def say_hello(name = "Ruby Programmer")
-  puts "Hello #{name}"
+  puts "Hello #{name}!"
 end
